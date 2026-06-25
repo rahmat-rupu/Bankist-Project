517e36d6a26321301d11dcf901ddece4e2ed71de
rahmat-rupu <rahmatrupu75@gmail.com>
2026-06-26 01:22:21 +0600

    added today,yesterday and days ago system

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2026-06-22T17:01:17.194Z',
+    '2026-06-24T23:36:17.929Z',
+    '2026-06-25T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -113,6 +113,24 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date1 - date2) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+};
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -124,10 +142,7 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
 
     const html = `
       <div class="movements__row">
@@ -177,6 +192,10 @@ const calcDisplaySummary = function (acc) {
 
 //Event Handlers
 let currentAccount;
+
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
 
 const now = new Date();
 const day = `${now.getDate()}`.padStart(2, 0);
@@ -475,7 +494,7 @@ movements.sort((a, b) => a - b);
 console.log(movements);
  */
 
-const x = new Array(7);
+/* const x = new Array(7);
 console.log(x);
 
 x.fill(1, 3, 5);
@@ -489,4 +508,4 @@ labelBalance.addEventListener('click', function () {
     document.querySelectorAll('.movements__value'),
   );
   console.log(movementsUI.map(el => el.textContent));
-});
+}); */
